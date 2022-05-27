1dd3458e7ede947d6205f03d9ade32c283c79425
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-05-27 23:03:22 +0100

    Matheus Battisti - Hora de Codar - CRIE UMA CALCULADORA COM HTML CSS E JAVASCRIPT

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,2 +1,3 @@
 const previousOperationText = document.querySelector("#previous-operation")
 const currentOperationText = document.querySelector("#current-operation")
+const buttons = document.querySelectorAll("#buttons-container button")
